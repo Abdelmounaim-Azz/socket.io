8e1d54a23692ab44ce4e05099cb2f918071c821e
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-04-02 17:47:46 +0100

    basics

---
+++ basics.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const socketio = require("socket.io");
+const server = http.createServer((req, res) => {
+  res.end("I am connected");
+});
+
+const io = socketio(server);
+io.on("connection", (socket, req) => {
+  socket.emit("welcome to socket server");
+  socket.on("message", (msg) => console.log(msg));
+});
+server.listen(8080);

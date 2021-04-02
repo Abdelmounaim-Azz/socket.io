c8c6764a12298ad04773b09a13b7ca071da6e735
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-04-02 18:37:25 +0100

    socketio basics

--- basics.js
+++ basics.js
@@ -1,12 +1,19 @@
 const http = require("http");
+
 const socketio = require("socket.io");
+
 const server = http.createServer((req, res) => {
-  res.end("I am connected");
+  res.end("server connected");
 });
 
 const io = socketio(server);
+
 io.on("connection", (socket, req) => {
-  socket.emit("welcome to socket server");
-  socket.on("message", (msg) => console.log(msg));
+  socket.emit("welcome", "Welcome to the websocket server!!");
+
+  socket.on("message", (msg) => {
+    console.log(msg);
+  });
 });
-server.listen(8080);
+
+server.listen(8000);

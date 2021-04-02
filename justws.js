3c04b367d739e3c0fbcbae095889aca0ffcfe27e
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-04-02 17:24:07 +0100

    nothing fancy

--- justws.js
+++ justws.js
@@ -5,7 +5,13 @@ const server = http.createServer((req, res) => {
 });
 //create ws server
 const wss = new websocket.Server({ server });
-wss.on("headers", (msg) => {
-  console.log(msg);
+//see headers before sending req
+wss.on("headers", (headers, req) => {
+  console.log(headers);
+});
+//afetr handshake
+wss.on("connection", (ws, req) => {
+  ws.send("welcome to ws server");
+  ws.on("message", (msg) => console.log(msg));
 });
 server.listen(5000);
